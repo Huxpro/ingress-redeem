536cc7cb424fdb4963fa2095ce45d646401d476e
黄玄 <huxpro@gmail.com>
2016-07-23 01:18:34 +0800

    First Commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Ingress Redeem 
+
+This is a browser script (a.k.a javascript) snippet for **continuously passcode redeeming** in game ingress.
+
+### How to use it
+
+1. Open the Web version of ingress: https://www.ingress.com/intel 
+2. Log in
+- Open Chrome DevTool - Console
+- Copy code from `index.js` and paste into the console
+- Press `Enter` to run 
+
+### API 
+
+`
+  /**
+   * Start Redeem Loop
+   * @param  {array}  passcodes
+   * @param  {number} interval
+   * @return
+   */
+  redeemLoop(passcodes, 5000);
+`
+
+Change passcodes to use your own, or change interval to run it faster or slower. 
+
+**WARNING:** Redeeming too fast will cause a system anti-robot strategy.
---
+++ index.js
@@ -0,0 +1,67 @@
+(function(){
+
+  var $passcode = document.getElementById('passcode');
+  var $submit = document.getElementById('submit');
+
+  function redeemOnce(passcode){
+    $passcode.value = passcode;
+    $submit.click()
+  }
+
+  function redeemLoop(passcodes, interval){
+    passcodeList = passcodes.split('\n');
+    console.log(passcodeList);
+
+    for (var i = 0; i < passcodeList.length; i++) {
+      setTimeout((function(){
+        let _i = i
+        return function(){
+          console.log(passcodeList[_i]);
+          redeemOnce(passcodeList[_i])
+        }
+      })(), interval * i)
+    }
+  }
+
+  var passcodes =`
+  ezekiel3xh34ug4
+  susanna3ku75cm9
+  LVBOYNXAIE
+  VI2JO15ND0
+  vi9bb02fk7
+  vi9rp62ex1
+  inveniri2hc78yy4
+  artifact4tt67xg9
+  roland8cx62mk4
+  evolution6xu68ru7
+  timezero2kk78gx5
+  hubert4su42qt2
+  lightman8nd48zb2
+  johnson3ba26qb2
+  jackland8vf92qz5
+  powercube3hu72ut7
+  glyph7jb25yw3
+  johnson3fx84aw9
+  symbols4ye57bs7
+  kureze3ft26jc6
+  blue2xc26da2
+  resonate6wb48ec4
+  MINOTAUR8DM83GG5
+  ezekiel7eu89au4
+  ingress9tu32jk7
+  ni7up28fu6
+  drone5sg25ez6
+  drone9rc88jy5
+  message5ka73rp4
+  80jdfitmar
+  CASSANDRA2YU35CP6
+  alignment3qh24up8`
+
+  /**
+   * Start Redeem Loop
+   * @param  {array}  passcodes
+   * @param  {number} interval
+   * @return
+   */
+  redeemLoop(passcodes, 5000);
+}())
